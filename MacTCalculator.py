34c67a540500725bcbbeffe259e03ec1b681a6b8
juanreyes2022 <juanreyesgeneral@gmail.com>
2024-10-15 00:11:52 -0400

    added error handling for nearly all cases, more print cleanup main issue is the program recognizing "pi" as its value and not a string while simultaneously allowing for fraction syntax other math functions will be added once these issues are resolved

--- MacTCalculator.py
+++ MacTCalculator.py
@@ -3,19 +3,24 @@ import math
 clear = lambda: os.system('clear')
 
 pi = 3.14159265359
+# divide menu sections
+line = 70
+trig = 35
+eX = 35
 
 def main():
     playerName = input('Please enter your name to begin: ')
-    print('Hello', playerName,', welcome to the MacT Calculator!')
+    print('Hello ' + str(playerName) + ', welcome to the MacT Calculator!')
 
     menu = 0
-    while menu != 4:
-        print("-"*35)
+    while menu != 5:
+        print("-"*line)
         print("Main Menu\
               \n1. Start the Calculator\
-              \n2. Learn about Taylor/Maclaurin series.\
-              \n3. Dedication & Contributors\
-              \n4. Quit.")
+              \n2. Program Information\
+              \n3. Learn about Taylor/Maclaurin series.\
+              \n4. Dedication & Contributors\
+              \n5. Quit.")
         
         menu = int(input("\nPlease select one of the options above: "))
 
@@ -24,70 +29,115 @@ def main():
         elif menu == 2:
             ShowInfo()
         elif menu == 3:
-            Special()
+            ShowLearn()
         elif menu == 4:
-            print('\nGoodbye. Thank you for using the Taylor calculator.')
+            Special()
+        elif menu == 5:
+            print('Shutting down ...\
+                  \nGoodbye ' + str(playerName) + ', and thank you for using the MacT calculator.')
         else:
             print('\nError ... Invalid option. Try again.')
 
 def Calc(playerName):
     calcMenu = 0
     while calcMenu != 8:
-        print("-"*35)
-        print(playerName, 'here is a list of all available functions.')
+        num = 0
+        type = ' '
+        print("-"*line)
         # easier way to alter list
         FunctionList()
-        
-        calcMenu = int(input("\nPlease select one of the options above: "))
+
+        try:
+            calcMenu = int(input('\n' + str(playerName) + ', please select one of the options above: '))
+        except ValueError:
+            print('Error ... Invalid selection. Try again.')
+
+        if calcMenu in [1, 2, 3, 4]:
+            print("~"*trig)
+        elif calcMenu in [5, 6, 7]:
+            print("*"*eX)
         
         if calcMenu == 1:
-            num = float(input('\nEnter your desired value: '))
-            type = str(input('\nEnter the unit of angle (deg or rad): '))
+            try:
+                num = Constant(Convert(input('Enter your desired value (sin x): ')))
+                type = str(input('Enter the unit of angle (deg or rad): '))
+            except ValueError:
+                print('Error ... Invalid input. Try again.')
+            except ZeroDivisionError:
+                print('Error ... Attempted division by zero. Try again.')
             # user's string input only allows for degree and radian units
             if type in ["deg", "rad"]:
-                sin(num, type)
+                sin(num, type, 0)
             else:
-                print('Error ... Invalid input. Try again.')
+                print('Error ... Invalid unit of angle. Try again.')
 
         elif calcMenu == 2:
-            num = float(input('\nEnter your desired value: '))
-            type = str(input('\nEnter the unit of angle (deg or rad): '))
+            try:
+                num = input('Enter your desired value (cos x): ')
+                num = Convert(num)
+                type = str(input('Enter the unit of angle (deg or rad): '))
+            except ValueError:
+                print('Error ... Invalid input. Try again.')
+            except ZeroDivisionError:
+                print('Error ... Attempted division by zero. Try again.')
             # user's string input only allows for degree and radian units
             if type in ["deg", "rad"]:
-                cos(num, type)
+                cos(num, type, 0)
             else:
-                print('Error ... Invalid input. Try again.')
+                print('Error ... Invalid unit of angle. Try again.')
 
         elif calcMenu == 3:
-            num = float(input('\nEnter your desired value: '))
-            type = str(input('\nEnter the unit of angle (deg or rad): '))
+            try:
+                num = input('Enter your desired value (tan x): ')
+                num = Convert(num)
+                type = str(input('Enter the unit of angle (deg or rad): '))
+            except ValueError:
+                print('Error ... Invalid input. Try again.')
+            except ZeroDivisionError:
+                print('Error ... Attempted division by zero. Try again.')
             # user's string input only allows for degree and radian units
             if type in ["deg", "rad"]:
                 tan(num, type)
             else:
-                print('Error ... Invalid input. Try again.')
+                print('Error ... Invalid unit of angle. Try again.')
 
         elif calcMenu == 4:
-            num = float(input('\nEnter your desired value: '))
-            type = str(input('\nEnter the unit of angle (deg or rad): '))
+            try:
+                num = input('Enter your desired value (arctan x): ')
+                num = Convert(num)
+                type = str(input('Enter the unit of angle (deg or rad): '))
+            except ValueError:
+                print('Error ... Invalid input. Try again.')
+            except ZeroDivisionError:
+                print('Error ... Attempted division by zero. Try again.')
             # user's string input only allows for degree and radian units
             if type in ["deg", "rad"]:
                 arctan(num, type)
             else:
-                print('Error ... Invalid input. Try again.')
+                print('Error ... Invalid unit of angle. Try again.')
 
         elif calcMenu == 5:
-            num = float(input('\nEnter your desired value: '))
+            try:
+                num = input('Enter your desired value (e^x): ')
+                num = Convert(num)
+            except ValueError:
+                print('Error ... Invalid input. Try again.')
+            except ZeroDivisionError:
+                print('Error ... Attempted division by zero. Try again.')
             e(num)
             
         elif calcMenu == 6 or calcMenu == 7:
-            num = float(input('\nEnter your desired value: '))
+            try:
+                num = input('Enter your desired value (ln 1 ± x): ')
+                num = Convert(num)
+            except ValueError:
+                print('Error ... Invalid input. Try again.')
+            except ZeroDivisionError:
+                print('Error ... Attempted division by zero. Try again.')
             ln(num, calcMenu)
 
         elif calcMenu == 8:
-            print('Returning to Main Menu...')
-        else:
-            print('Error ... Invalid selection. Try again.')
+            print('Returning to Main Menu ...')
 
 #The trig functions take a number then the unit of that number "rad" or "deg" after a comma
 # Ex sin(45,"deg"), tan(pi/3,"rad")
@@ -105,9 +155,10 @@ def sin (num, type, n):
         computed_result += ((-1) ** n) * (num ** (2 * n+1))/math.factorial(2 * n+1)
         n += 1
 
-    print(f"sin({num}) =", computed_result, f". After {n} terms of its\
-          \nMaclaurin expansion it is less than or equal to the correct\
-          \nanswer, within a 0.00001 ERROR")
+    print("~"*trig)
+    print(f"\nsin({num}) =", computed_result)
+    print(f"After {n} term(s) of its Maclaurin expansion it is less than\
+            \nor equal to the correct answer, within a 0.00001 ERROR")
     
     return computed_result, n
 
@@ -122,23 +173,26 @@ def cos(num, type, n):
         computed_result += ((-1) ** n) * (num ** (2 * n))/math.factorial(2 * n)
         n += 1
 
-    print(f"cos({num}) =", computed_result, f". After {n} terms of its\
-          \nMaclaurin expansion it is less than or equal to the correct\
-          \nanswer, within a 0.00001 ERROR")
+    print("~"*trig)
+    print(f"\ncos({num}) =", computed_result)
+    print(f"After {n} term(s) of its Maclaurin expansion it is less than\
+            \nor equal to the correct answer, within a 0.00001 ERROR")
 
     return computed_result, n
 
 def tan(num, type):
     #create values to compare or add too
     n = 0
+    # deg to rad conversion is not included as it is done in the sin and cos functions
     result = math.tan(num)
     while computed_result <= result - 0.00001 or computed_result >= result + .00001:
         computed_result = sin(num, type, n) / cos(num, type, n)
         n += 1
 
-    print(f"tan({num}) =", computed_result, f". After {n} terms of its\
-          \nMaclaurin expansion it is less than or equal to the correct\
-          \nanswer, within a 0.00001 ERROR")
+    print("~"*trig)
+    print(f"\ntan({num}) =", computed_result)
+    print(f"After {n} term(s) of its Maclaurin expansion it is less than\
+            \nor equal to the correct answer, within a 0.00001 ERROR")
 
 def arctan(num, type):
     computed_result = 0.0
@@ -151,9 +205,10 @@ def arctan(num, type):
         computed_result += ((-1) ** n) * (num ** (2 * n+1)) / (2 * n+1)
         n += 1 
 
-    print(f"arctan({num}) =", computed_result, f". After {n} terms of its\
-          \nMaclaurin expansion it is less than or equal to the correct\
-          \nanswer, within a 0.00001 ERROR")
+    print("~"*trig)
+    print(f"\narctan({num}) =", computed_result)
+    print(f"After {n} term(s) of its Maclaurin expansion it is less than\
+            \nor equal to the correct answer, within a 0.00001 ERROR")
     
 def e(num):
     computed_result = 0.0
@@ -164,9 +219,10 @@ def e(num):
         computed_result += (num**(n))/math.factorial(n)
         n += 1
 
-    print(f"e^{num} =", computed_result, f". After {n} terms of its\
-          \nMaclaurin expansion it is less than or equal to the correct\
-          \nanswer, within a 0.00001 ERROR")
+    print("*"*eX)
+    print(f"\ne^{num} =", computed_result)
+    print(f"After {n} term(s) of its Maclaurin expansion it is less than\
+            \nor equal to the correct answer, within a 0.00001 ERROR")
 
 def ln(num, calcMenu):
     computed_result = 0.0
@@ -178,10 +234,11 @@ def ln(num, calcMenu):
             computed_result += ((-1) ** (n + 1)) * (num ** n) / n
             print(computed_result)
             n += 1
-        
-        print(f"ln(1 + {num}) =", computed_result, f". After {n} terms of its\
-          \nMaclaurin expansion it is less than or equal to the correct\
-          \nanswer, within a 0.00001 ERROR")
+
+        print("*"*eX)
+        print(f"\nln(1 + {num}) =", computed_result)
+        print(f"After {n} term(s) of its Maclaurin expansion it is less than\
+                \nor equal to the correct answer, within a 0.00001 ERROR")
 
     elif calcMenu == 7:
         result = math.log(1-num)
@@ -190,61 +247,88 @@ def ln(num, calcMenu):
             print(computed_result)
             n += 1
         
-        print(f"ln(1 - {num}) =", computed_result, f". After {n} terms of its\
-          \nMaclaurin expansion it is less than or equal to the correct\
-          \nanswer, within a 0.00001 ERROR")
+        print("*"*eX)
+        print(f"\nln(1 - {num}) =", computed_result)
+        print(f"After {n} term(s) of its Maclaurin expansion it is less than\
+                \nor equal to the correct answer, within a 0.00001 ERROR")
+
+# let user input fractions and convert to actual values
+def Convert(num):
+    if '/' not in str(num):
+        return float(num)
+    else:
+        fract = num.split('/')
+        return float(fract[0])/float(fract[1])
+
+# these two functions ^ v have to be merged somehow to allow for fractions to work as well as "pi" be recognized as an input
+
+def Constant(num):
+    constants = {"pi": pi, "e": 2.718281828}
+    if num in constants:
+        return constants[num]
+    else:
+        return float(num)
 
 # separate function if more math functions added
 def FunctionList():
-    print("\nFunction Menu\
-           \n\
-           \nTrigonometric Function Expansions:\
+    print("Function Menu")
+    print("~"*trig)
+    print("Trigonometric Function Expansions:\
            \n1. Sine\
            \n2. Cosine\
            \n3. Tangent\
-           \n4. Arctangent\
-           \n\
-           \nGrowth and Decay Function Expansions:\
+           \n4. Arctangent")
+    print("~"*trig)
+    print("*"*eX)
+    print("Growth and Decay Function Expansions:\
            \n5. Euler's Number (e)\
            \n6. Natural Logarithm (1 + x)\
-           \n7. Natural Logarithm (1 - x)\
-           \n8. Return to Main Menu")
+           \n7. Natural Logarithm (1 - x)")
+    print("*"*eX)
+    print("To return to the Main Menu, enter 8.")
 
 def ShowInfo():
-    print("-"*35)
-    print("\nThe MacT Calculator uses material covered in Calculus II\
-           \nregarding the Taylor series of a function. The concept is\
-           \nnamed after the English mathematician Brook Taylor (1685\
-           \n-1731), who introduced the series in 1715.\
-           \n\
-           \nIn short, the Taylor series of a function is a polynomial of\
-           \nan infinite sum of terms expressed through the function's\
-           \nderivatives at a single point. This means that for most\
-           \ncommon functions, the function and the sum of its Taylor\
-           \nseries are equal at that point.\
-           \n\
-           \nThe logic of this program uses a variation of the Taylor\
-           \nseries, where the point chosen is 0. This expansion is known\
-           \nas a Maclaurin series, named after the Scottish mathematican\
-           \nColin Maclaurin (1698-1746).\
-           \n\
-           \nIn this application, several functions are available to you,\
+    print("-"*line)
+    print("In this application, several functions are available to you,\
            \nand the program calculates the answer by the function's\
            \nMaclaurin expansion and the built-in functions of the math library.\
            \n\
            \nAt the end of the calculations, the program will determine how\
-           \nmany terms the Maclaurin series needs to reach the correct answer.")
+           \nmany terms the Maclaurin series needs to reach the correct answer.\
+           \n\
+           \nEach function will require the user to input a value to calculate,\
+           \nbut the trigonometric functions require you to specify if\
+           \nthe unit is degrees or radians. The syntax for entering the angle is\
+           \n\"deg\" or \"rad\".\
+           \n\
+           \nTo enter fractions, the proper syntax is shown: a/b.")
+
+
+def ShowLearn():
+    print("-"*line)
+    print("The MacT Calculator uses material covered in Calculus II regarding\
+           \nthe Taylor series of a function. The concept is named after the\
+           \nEnglish mathematician Brook Taylor (1685-1731), who introduced\
+           \nthe series in 1715.\
+           \n\
+           \nIn short, the Taylor series of a function is a polynomial of an\
+           \ninfinite sum of terms expressed through the function's derivatives\
+           \nat a single point. This means that for most common functions, the\
+           \nfunction and the sum of its Taylor series are equal at that point.\
+           \n\
+           \nThe logic of this program uses a variation of the Taylor series,\
+           \nwhere the point chosen is 0. This expansion is known as a Maclaurin\
+           \nseries, named after the Scottish mathematican Colin Maclaurin (1698-1746).")
     
 def Special():
-    print("-"*35)
-    print("\nThis program orignally began as an optional route for\
-           \na Calculus II project, but I plan to integrate this\
-           \ninto a framework and proper web application.\
+    print("-"*line)
+    print("This program orignally began as an optional route for a Calculus II\
+           \nproject, but I plan to integrate this into a framework\
+           \n and proper web application.\
            \n\
-           \nSpecial thanks to Antoine for designing some of\
-           \nthe functions and their Maclaurin series, and Alex\
-           \nfor the numerous study sessions we had when reaching\
-           \nthe \"Infinite Series\" section. I could not have\
+           \nSpecial thanks to Antoine for designing some of the functions\
+           \nand their Maclaurin series, and Alex for the numerous study\
+           \nsessions we had during the semester. I could not have\
            \npassed Calculus without you guys!")
 
 if __name__ == '__main__':
